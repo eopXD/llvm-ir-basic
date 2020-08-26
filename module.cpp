a5014148768f6c12d2c7177e0fe00eca0c52f18b
eopXD <yueh.ting.chen@gmail.com>
2020-08-26 19:09:55 +0800

    declare an empty function

--- module.cpp
+++ module.cpp
@@ -2,17 +2,30 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Verifier.h"
 
+#include <vector>
 using namespace llvm;
 
 static Module *TheModule;
 static LLVMContext TheContext;
 static IRBuilder<> Builder(TheContext);
 
-int main ()
+
+Function *createFunc(IRBuilder<> &Builder, std::string Name) {
+	FunctionType *funcType = llvm::FunctionType::get(Builder.getInt32Ty(), false);
+	Function *fooFunc = llvm::Function::Create(
+	funcType, llvm::Function::ExternalLinkage, Name, TheModule);
+	return fooFunc;
+}
+int main ( int argc, char *argv[] )
 {
 	llvm::LLVMContext &Context = TheContext;
 	TheModule = new Module("my compiler", Context);
+	Function *fooFunc = createFunc(Builder, "foo");
+
+	verifyFunction(*fooFunc);
 	TheModule->print(llvm::outs(), nullptr);
 	return 0;
+
 }
